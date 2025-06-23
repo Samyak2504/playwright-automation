3f8d27cbf37559f4c708b03356f77855e80de430
Samyak2504 <samyak@10times.com>
2025-06-23 13:59:33 +0530

    Update edit_company_profile_business_website.py

--- scripts/edit_company_profile_business_website.py
+++ scripts/edit_company_profile_business_website.py
@@ -1,8 +1,7 @@
 import re
-from playwright.sync_api import Playwright, async_playwright
+from playwright.sync_api import Playwright, sync_playwright
 
-
-def run(playwright: Playwright) -> None:
+def run_script(playwright: Playwright) -> None:
     browser = playwright.firefox.launch(headless=True, slow_mo=2000)
     context = browser.new_context()
     page = context.new_page()
@@ -19,33 +18,24 @@ def run(playwright: Playwright) -> None:
 
     page.locator("(//a[@class='btn btn-default'])").click()
     page.locator("(//a[@class='btn btn-default btn-xs profileAcc'])[1]").click()
-
     page.locator("(//a[@class='introjs-skipbutton'])").click()
 
-
     # Change the value of the Website input using XPath
-    address_input = page.locator("(//div[@class='col-sm-4']/input[@id='website'])")
-    address_input.click()
-    address_input.fill("https://xyz6567.com")  # Change the value here
+    website_input = page.locator("(//div[@class='col-sm-4']/input[@id='website'])")
+    website_input.click()
+    website_input.fill("https://xyz6567.com")  # Change the value here
 
     page.locator("(//div[@class='box-footer']/button[text()='Save'])").click()
     page.locator("(//div[@id='alert']//button[@id='ok-button' and text()='Ok'])").click()
 
-    # Optional: wait to see the result
     page.wait_for_timeout(3000)
 
-    # ---------------------
     context.close()
     browser.close()
 
-
 def run() -> None:
     with sync_playwright() as playwright:
-        run(playwright)
-
-
-# Run the main async function
-run()
+        run_script(playwright)
 
 if __name__ == "__main__":
     run()
