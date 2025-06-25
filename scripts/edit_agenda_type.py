e5c5b61be4b7013fd3dac33123ca0029d04aee19
Samyak2504 <samyak@10times.com>
2025-06-25 17:10:20 +0530

    Update edit_agenda_type.py

--- scripts/edit_agenda_type.py
+++ scripts/edit_agenda_type.py
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
-=======
-<<<<<<<< HEAD:scripts/e_unpublised_agenda.py
->>>>>>> 8840c4d9671bdf7019e6c9a4e489368757a98eda
 from playwright.sync_api import Playwright, sync_playwright
 
 
 def run_script(playwright: Playwright) -> None:
     browser = playwright.firefox.launch(headless=True, slow_mo=2000)
-<<<<<<< HEAD
-=======
-========
+
+
 import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run_script(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=True, slow_mo=2000)
->>>>>>>> 8840c4d9671bdf7019e6c9a4e489368757a98eda:scripts/edit_agenda_type.py
->>>>>>> 8840c4d9671bdf7019e6c9a4e489368757a98eda
     context = browser.new_context()
     page = context.new_page()
 
@@ -30,16 +23,14 @@ def run_script(playwright: Playwright) -> None:
     page.get_by_role("button", name="Login to your account").click()
     page.get_by_role("button", name="Close").click()
     page.goto("https://login.10times.com/event/eadmin/1154170/agenda")
-<<<<<<< HEAD
     page.locator("(//input[@id='cb1'])").click()
     page.locator("(//button[text()='Edit'])[1]").click()
     page.get_by_placeholder("Session Type").click()
     page.get_by_role("link", name="Closing Ceremony", exact=True).click()
     page.get_by_role("button", name="Save").click()
     page.get_by_role("link", name="Listing").click()
-=======
+
     page.locator("//label[@class='tgl-btn' and @for='b731313'][1]").click()
->>>>>>> 8840c4d9671bdf7019e6c9a4e489368757a98eda
 
     # ---------------------
     context.close()
@@ -51,12 +42,6 @@ def run() -> None:
         run_script(playwright)
 
 
-<<<<<<< HEAD
-=======
-<<<<<<<< HEAD:scripts/e_unpublised_agenda.py
-========
 
->>>>>>>> 8840c4d9671bdf7019e6c9a4e489368757a98eda:scripts/edit_agenda_type.py
->>>>>>> 8840c4d9671bdf7019e6c9a4e489368757a98eda
 if __name__ == "__main__":
     run()
