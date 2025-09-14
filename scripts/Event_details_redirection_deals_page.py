9a4c1ebe2202db10eca95cceb0fc448a8e8eeb74
Samyak Kumar <samyak@10times.com>
2025-09-15 02:25:46 +0530

    details filling

---
+++ scripts/Event_details_redirection_deals_page.py
@@ -0,0 +1,33 @@
+from playwright.sync_api import sync_playwright
+import time
+
+def get_temp_email_and_otp():
+    with sync_playwright() as p:
+        # ✅ Launch browser
+        browser = p.firefox.launch(headless=True, slow_mo=1000)
+
+        # ✅ Define custom user-agent
+        custom_user_agent = "TenTimes internal Testing/tentimestesting10t112"
+
+        # ✅ Create context with user-agent
+        context = browser.new_context(
+            user_agent=custom_user_agent,
+            extra_http_headers={"User-Agent": custom_user_agent}
+        )
+
+        # ✅ Create page from context
+        page = context.new_page()
+        page.goto("https://10times.com/event/928626")
+
+        # ✅ Click the Followers
+        page.locator("//span[text()='Deals']").click()
+
+        page.wait_for_timeout(5000)
+
+        # ✅ Close context and browser
+        context.close()
+        browser.close()
+
+# Run the function
+if __name__ == "__main__":
+    get_temp_email_and_otp()
