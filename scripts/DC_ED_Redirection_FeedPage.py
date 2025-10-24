3785269d4d920cdf70e5cbb50d7be1b9fbea33aa
Samyak Kumar <samyak@10times.com>
2025-10-24 16:57:22 +0530

    Locatar change

--- scripts/DC_ED_Redirection_FeedPage.py
+++ scripts/DC_ED_Redirection_FeedPage.py
@@ -3,28 +3,28 @@ import time
 
 def get_temp_email_and_otp():
     with sync_playwright() as p:
-        # ✅ Launch browser
+        # Launch browser
         browser = p.firefox.launch(headless=True, slow_mo=1000)
 
-        # ✅ Define custom user-agent
+        #  Define custom user-agent
         custom_user_agent = "TenTimes internal Testing/tentimestesting10t112"
 
-        # ✅ Create context with user-agent
+        #  Create context with user-agent
         context = browser.new_context(
             user_agent=custom_user_agent,
             extra_http_headers={"User-Agent": custom_user_agent}
         )
 
-        # ✅ Create page from context
+        #  Create page from context
         page = context.new_page()
         page.goto("https://10times.com/magic")
 
-        # ✅ Click the Feed
-        page.locator("//a[contains(@class,'tt-tabs-other') and text()='Feed']").click()
+        #  Click the Feed
+        page.locator("//td[@id='hubs']/a[normalize-space(text())='Feed']").click()
 
         page.wait_for_timeout(5000)
 
-        # ✅ Close context and browser
+        #  Close context and browser
         context.close()
         browser.close()
 
